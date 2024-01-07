5ae0ef80560083d4577c09e096678c9c24760260
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2024-01-07 20:25:50 -0300

    Initial Commit

---
+++ medanospaper/medanospaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  medanospaperApp.swift
+//  medanospaper
+//
+//  Created by Agustin Carbajal on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct medanospaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
